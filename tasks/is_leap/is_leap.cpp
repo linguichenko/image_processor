b3d8b556d58ead69c1a05ab118395cc4fe76df24
Darya Linichenko <dalinichenko@edu.hse.ru>
2024-01-26 17:07:10 +0300

    adding ;

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -4,5 +4,5 @@ bool IsLeap(int year) {
     const int four = 4;
     const int hundred = 100;
     const int four_hundreds = 400;
-    return (year % four == 0 && year % hundred != 0) || (year % four_hundreds == 0)
+    return (year % four == 0 && year % hundred != 0) || (year % four_hundreds == 0);
 }
