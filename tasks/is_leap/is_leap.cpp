94bcc989e5c2d3837e19db41a4a32b8f63dada25
Darya Linichenko <dalinichenko@edu.hse.ru>
2024-01-26 17:02:34 +0300

    happy leap year

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,8 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    return true;
+    const int four = 4;
+    const int hundred = 100;
+    const int four_hundreds = 400;
+    return (year % four == 0 && year % hundred != 0) || (year % four_hundreds == 0)
 }
