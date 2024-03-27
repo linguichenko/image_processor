87ad896e9f9d6f6ef47dbd2795cdaa10124846fb
Darya Linichenko <dalinichenko@edu.hse.ru>
2024-03-27 22:21:19 +0300

    cycle change

--- tasks/image_processor/image_processor.cpp
+++ tasks/image_processor/image_processor.cpp
@@ -21,20 +21,26 @@ int main(int argc, char* argv[]) {
     Image image;
     image.Read(args.input_file);
     std::vector<FilterArgs> filters;
+    std::cout<<argc<<std::endl;
     int i = 3;
+    int k = 0;
     while (i < argc) {
+        std::cout <<"i: "<<i<<std::endl;
         if (argv[i][0] == '-') {
             FilterArgs new_args;
             new_args.name = argv[i];
             filters.push_back(new_args);
             int j = i + 1;
             while (j < argc && argv[j][0] != '-') {
-                filters[i - 3].parameters.push_back(argv[j]);
+                std::cout<<"j "<<j<<std::endl;
+                filters[k].parameters.push_back(argv[j]);
                 ++j;
             }
             i = j;
+            ++k;
         }
     }
+    std::cout << "something" << std::endl;
     for (FilterArgs const& arg : filters) {
         if (arg.name == "-gs") {
             GreyscaleFactory factory;
@@ -57,9 +63,11 @@ int main(int argc, char* argv[]) {
             ptr->ApplySpecial(image);
         }
         if (arg.name == "-edge") {
+            std::cout<<1<<std::endl;
             EdgesFactory factory;
             std::unique_ptr<FilterWithMatrix> ptr = factory.CreateMatrix(arg.parameters);
             ptr->ApplySpecial(image);
+            std::cout<<2<<std::endl;
         }
         if (arg.name == "-blur") {
             BlurFactory factory;
