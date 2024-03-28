142dc34f9af937fd7dc247a17d69a35f40b728f7
Darya Linichenko <dalinichenko@edu.hse.ru>
2024-03-28 13:07:52 +0300

    mini corrections

--- tasks/image_processor/filters/grayscale/greyscale.cpp
+++ tasks/image_processor/filters/grayscale/greyscale.cpp
@@ -7,8 +7,8 @@ void Greyscale::Apply(Image &image) const {
     const float blue = 0.114;
     for (int i = 0; i < image.GetHeight(); ++i) {
         for (int j = 0; j < image.GetWidth(); ++j) {
-            float color = 0;
-            color = red * image.GetColor(i, j).r + green * image.GetColor(i, j).g + blue * image.GetColor(i, j).b;
+            Color curr_color = image.GetColor(i, j);
+            float color = red * curr_color.r + green * curr_color.g + blue * curr_color.b;
             image.SetColor(color, color, color, i, j);
         }
     }
--- tasks/image_processor/filters/negative/negative.cpp
+++ tasks/image_processor/filters/negative/negative.cpp
@@ -4,9 +4,10 @@
 void Negative::Apply(Image &image) const {
     for (int i = 0; i < image.GetHeight(); ++i) {
         for (int j = 0; j < image.GetWidth(); ++j) {
-            float red = 1 - image.GetColor(i, j).r;
-            float green = 1 - image.GetColor(i, j).g;
-            float blue = 1 - image.GetColor(i, j).b;
+            Color curr_color = image.GetColor(i, j);
+            float red = 1 - curr_color.r;
+            float green = 1 - curr_color.g;
+            float blue = 1 - curr_color.b;
             image.SetColor(red, green, blue, i, j);
         }
     }
